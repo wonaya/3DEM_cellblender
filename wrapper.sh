beb64ffd4d9646c00083404113e103dcac7f6fd3
Jawon Song <wonaya@c461-103.stampede2.tacc.utexas.edu>
2021-06-22 08:53:10 -0500

    added input files

---
+++ wrapper.sh
@@ -0,0 +1,5 @@
+######### START CUSTOM INSTALL SCRIPT ############
+WORKDIR=${workingDirectory}
+
+module load swr
+swr -t 16 /work2/projects/NeuroNex/3DEM/bin/Blender/Blender-2.79-CellBlender/blender
